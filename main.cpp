371a96b8ef39605cdc23cb70282094656eb41d29
Nedim Badzak <nbadzak1@etf.unsa.ba>
2021-12-08 19:31:50 +0100

    Day 3 done

--- main.cpp
+++ main.cpp
@@ -2,8 +2,42 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include <cmath>
 
 using namespace std;
+
+void kickOut(vector<string> &vector1, int j, int n);
+
+long binaryToDecimal(const string &s) {
+    long n = stol(s);
+    long decimal = 0;
+
+    for (int i = 0; i < s.length(); i++) {
+        decimal += n % 10 * pow(2, i);
+        n /= 10;
+    }
+    return decimal;
+}
+
+bool returnMostCommonBit(vector<string> &v, int index) {
+    int zeroes = 0;
+    int ones = 0;
+    for (int j = 0; j < v.size(); ++j) {
+        if (v[j][index] == '0') {
+            zeroes++;
+        }
+        if (v[j][index] == '1') {
+            ones++;
+        }
+    }
+    if (zeroes > ones) {
+        return false;
+    }
+
+    if (ones >= zeroes) {
+        return true;
+    }
+}
 
 int main() {
     std::string myText;
@@ -11,35 +45,79 @@ int main() {
     std::ifstream MyReadFile("/home/nedim/CLionProjects/AdventOfCode/input.txt");
 //    std::ifstream MyReadFile("/home/nedim/CLionProjects/AdventOfCode/input2.txt");
 
-    vector<vector<string>> v;
 
+    vector<string> numbers;
     while (getline(MyReadFile, myText)) {
-        vector<string> tempVector;
-        for (int i = 0; i < myText.size(); i++) {
-            if (myText[i] == ' ') {
-                tempVector.push_back(myText.substr(0, i));
-                tempVector.push_back(myText.substr(i + 1));
+        numbers.push_back(myText);
+    }
+
+
+    string gammaRate = "";
+    string epsilonRate = "";
+    for (int i = 0; i < numbers[0].size(); ++i) {
+        int zeroes = 0;
+        int ones = 0;
+        for (int j = 0; j < numbers.size(); ++j) {
+            if (numbers[j][i] == '0') {
+                zeroes++;
+            }
+            if (numbers[j][i] == '1') {
+                ones++;
             }
         }
-        v.push_back(tempVector);
-    }
+        gammaRate.push_back(zeroes > ones ? '0' : '1');
+        epsilonRate.push_back(zeroes > ones ? '1' : '0');
 
-    int horizontal = 0;
-    int depth = 0;
-    int aim = 0;
-    for (int i = 0; i < v.size(); i++) {
-        if (v.at(i).at(0) == "forward") {
-            horizontal += stoi(v.at(i).at(1));
-            depth += aim * stoi(v.at(i).at(1));
-        } else if (v.at(i).at(0) == "up") {
-            aim -= stoi(v.at(i).at(1));
-        } else if (v.at(i).at(0) == "down") {
-            aim += stoi(v.at(i).at(1));
+    }
+    cout << gammaRate << endl;
+    cout << epsilonRate << endl;
+    cout << "Power consumption: " << "gamma: " << binaryToDecimal(gammaRate) << " * epsilon: "
+         << binaryToDecimal(epsilonRate) << " = " << binaryToDecimal(gammaRate) *
+                                                     binaryToDecimal(epsilonRate) << endl;
+
+    vector<string> o2;
+    vector<string> co2;
+    std::copy(numbers.begin(), numbers.end(), std::back_inserter(o2));
+    std::copy(numbers.begin(), numbers.end(), std::back_inserter(co2));
+    for (int i = 0; i < o2[0].size(); ++i) {
+        if (returnMostCommonBit(o2, i)) {
+            kickOut(o2, i, 0);
+        } else {
+            kickOut(o2, i, 1);
+        }
+        if (o2.size() == 1) {
+            break;
         }
     }
-    std::cout << "Horizontal " << horizontal << " multiplied by depth " << depth << " equals " << horizontal * depth;
+    for (int i = 0; i < co2[0].size(); ++i) {
+        if (returnMostCommonBit(co2, i)) {
+            kickOut(co2, i, 1);
+        } else {
+            kickOut(co2, i, 0);
+        }
+        if (co2.size() == 1) {
+            break;
+        }
+    }
+
+    cout << "O2: " << binaryToDecimal(o2[0]) << endl;
+    cout << "CO2: " << binaryToDecimal(co2[0]) << endl;
+    cout << "Quality: " << binaryToDecimal(o2[0]) * binaryToDecimal(co2[0]) << endl;
+
 // Close the file
     MyReadFile.close();
-
-
+    return 0;
 }
+
+void kickOut(vector<string> &vector1, int j, int n) {
+    int size = vector1.size();
+    int it = 0;
+    for (int i = 0; i < size-it; i++) {
+        if (vector1[i][j] - '0' == n) {
+            vector1.erase(vector1.begin() + i);
+            i=-1;
+            it++;
+        }
+    }
+}
+
