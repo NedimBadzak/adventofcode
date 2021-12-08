a4181a79b555192f0f527c8a1a707e0ccd380094
Nedim Badzak <nbadzak1@etf.unsa.ba>
2021-12-08 18:02:36 +0100

    Day 2 done

--- main.cpp
+++ main.cpp
@@ -1,42 +1,45 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
 
 using namespace std;
+
 int main() {
-	std::string myText;
+    std::string myText;
 
-std::ifstream MyReadFile("input2.txt");
+    std::ifstream MyReadFile("/home/nedim/CLionProjects/AdventOfCode/input.txt");
+//    std::ifstream MyReadFile("/home/nedim/CLionProjects/AdventOfCode/input2.txt");
 
+    vector<vector<string>> v;
 
-int counter = 0;
-int counterZbirova = 0;
-int previous1 = 0;
-int previous2 = 0;
-int previous3 = 0;
+    while (getline(MyReadFile, myText)) {
+        vector<string> tempVector;
+        for (int i = 0; i < myText.size(); i++) {
+            if (myText[i] == ' ') {
+                tempVector.push_back(myText.substr(0, i));
+                tempVector.push_back(myText.substr(i + 1));
+            }
+        }
+        v.push_back(tempVector);
+    }
 
-
-while (getline (MyReadFile, myText)) {
-  /* if (stoi(myText) > previous) counter++; */
-  /* previous = stoi(myText); */
-
-  if (counter == 0) previous1 = stoi(myText);
-  else if (counter == 1) previous2 = stoi(myText);
-  else if (counter == 2)  previous3 = stoi(myText); 
-  else {
-	if(stoi(myText)+previous2+previous3 > previous1+previous2+previous3) counterZbirova++;
-	previous1 = previous2;
-	previous2 = previous3;
-	previous3 = stoi(myText);
-  }
-  counter++;
-}
-
-cout << "Broj povecanja je : " << counterZbirova;
-
-
+    int horizontal = 0;
+    int depth = 0;
+    int aim = 0;
+    for (int i = 0; i < v.size(); i++) {
+        if (v.at(i).at(0) == "forward") {
+            horizontal += stoi(v.at(i).at(1));
+            depth += aim * stoi(v.at(i).at(1));
+        } else if (v.at(i).at(0) == "up") {
+            aim -= stoi(v.at(i).at(1));
+        } else if (v.at(i).at(0) == "down") {
+            aim += stoi(v.at(i).at(1));
+        }
+    }
+    std::cout << "Horizontal " << horizontal << " multiplied by depth " << depth << " equals " << horizontal * depth;
 // Close the file
-MyReadFile.close();	
+    MyReadFile.close();
 
 
 }
